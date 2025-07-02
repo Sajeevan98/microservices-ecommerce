a9da86ccd5535bdf8b8116985256f56c9770482e
Sajeevan98 <sajee91098@gmail.com>
2025-07-02 23:57:10 +0530

    #3.7, add ProductService interface and implementation

---
+++ e-commerce/server/product-service/src/main/java/com/project/sajee/product/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.project.sajee.product.service;
+
+import com.project.sajee.product.dto.ProductRequest;
+
+public interface ProductService {
+    void createProducts(ProductRequest products);
+}
---
+++ e-commerce/server/product-service/src/main/java/com/project/sajee/product/service/imp/ProductServiceImp.java
@@ -0,0 +1,14 @@
+package com.project.sajee.product.service.imp;
+
+import com.project.sajee.product.dto.ProductRequest;
+import com.project.sajee.product.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImp implements ProductService {
+
+    @Override
+    public void createProducts(ProductRequest products) {
+
+    }
+}
