2ecc92b4c620296de4b854a0aba42fbb6c2cc18c
Sajeevan98 <sajee91098@gmail.com>
2025-07-03 22:35:29 +0530

    #5.3, create Order entity inside model package

---
+++ e-commerce/server/order-service/src/main/java/com/project/sajee/order/model/Order.java
@@ -0,0 +1,27 @@
+package com.project.sajee.order.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_data")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+    private String code;
+    private BigDecimal totalPrice;
+    private Integer quantity;
+}
