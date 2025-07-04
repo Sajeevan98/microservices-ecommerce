664fd5d5ce83a1da387151d525715d925e56f9ba
Sajeevan98 <sajee91098@gmail.com>
2025-07-04 21:56:25 +0530

    #7.0, add InventoryService interface and implementation

---
+++ e-commerce/server/inventory-service/src/main/java/com/project/sajee/inventory/service/InventoryService.java
@@ -0,0 +1,6 @@
+package com.project.sajee.inventory.service;
+
+public interface InventoryService {
+
+    boolean isStock(String code, Integer quantity);
+}
---
+++ e-commerce/server/inventory-service/src/main/java/com/project/sajee/inventory/service/imp/InventoryServiceImp.java
@@ -0,0 +1,18 @@
+package com.project.sajee.inventory.service.imp;
+
+import com.project.sajee.inventory.repository.InventoryRepository;
+import com.project.sajee.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryServiceImp implements InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Override
+    public boolean isStock(String code, Integer quantity) {
+        return false;
+    }
+}
