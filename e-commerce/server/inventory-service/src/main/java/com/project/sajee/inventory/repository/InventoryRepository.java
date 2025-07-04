35b0fac42ebf442275530a13e1a3c7a55e25b0bf
Sajeevan98 <sajee91098@gmail.com>
2025-07-04 21:54:30 +0530

    #6.9, create InventoryRepository interface in repository package

---
+++ e-commerce/server/inventory-service/src/main/java/com/project/sajee/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.project.sajee.inventory.repository;
+
+import com.project.sajee.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+}
