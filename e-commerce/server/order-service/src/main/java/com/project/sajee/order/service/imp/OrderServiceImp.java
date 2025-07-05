94feb8660fe8444c738e5519eea42a8cbf5c9a31
Sajeevan98 <sajee91098@gmail.com>
2025-07-05 14:14:07 +0530

    #7.9, implement Feign client interface for external service communication

--- e-commerce/server/order-service/src/main/java/com/project/sajee/order/service/imp/OrderServiceImp.java
+++ e-commerce/server/order-service/src/main/java/com/project/sajee/order/service/imp/OrderServiceImp.java
@@ -1,6 +1,8 @@
 package com.project.sajee.order.service.imp;
 
+import com.project.sajee.order.client.InventoryClient;
 import com.project.sajee.order.dto.OrderRequest;
+import com.project.sajee.order.exception.ProductOutOfStockException;
 import com.project.sajee.order.model.Order;
 import com.project.sajee.order.repository.OrderRepository;
 import com.project.sajee.order.service.OrderService;
@@ -16,23 +18,28 @@ import java.util.UUID;
 public class OrderServiceImp implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     @Override
     public Order placeOrder(OrderRequest order) {
-        if (order != null){
-            // map OrderRequest into OrderObject
-            Order orderObj = new Order();
-            orderObj.setOrderNumber(UUID.randomUUID().toString());
-            orderObj.setCode(order.code());
-            orderObj.setPrice(order.price());
-            orderObj.setQuantity(order.quantity());
+        if (order != null) {
+            var response = inventoryClient.isStock(order.code(), order.quantity());
+            if (response) {
+                // map OrderRequest into OrderObject
+                Order orderObj = new Order();
+                orderObj.setOrderNumber(UUID.randomUUID().toString());
+                orderObj.setCode(order.code());
+                orderObj.setPrice(order.price());
+                orderObj.setQuantity(order.quantity());
 
-            // save Order into OrderRepository
-            orderRepository.save(orderObj);
-            log.info("order placed successfully!");
-            return orderObj;
+                // save Order into OrderRepository
+                orderRepository.save(orderObj);
+                return orderObj;
+            }
+            else {
+                throw new ProductOutOfStockException("Product exists with code '"+order.code()+ "' not in stock!");
+            }
         }
-        log.info("something went wrong! could not placed the order.");
         return null;
     }
 
