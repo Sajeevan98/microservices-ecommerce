eaaff72bd6c3e6043f5446ebc50c82cb42d5605d
Sajeevan98 <sajee91098@gmail.com>
2025-07-05 15:23:53 +0530

    #8.4, create InventoryClient stub class with mock method for testing

---
+++ e-commerce/server/order-service/src/test/java/com/project/sajee/order/stub/InventoryClientStup.java
@@ -0,0 +1,17 @@
+package com.project.sajee.order.stub;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class InventoryClientStup {
+
+    public static void callForPositive(String code, Integer quantity) {
+        stubFor(
+                get(urlEqualTo("/api/v1.0/inventory?code=" + code + "&quantity=" + quantity))
+                        .willReturn(aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody("true")
+                        )
+        );
+    }
+}
