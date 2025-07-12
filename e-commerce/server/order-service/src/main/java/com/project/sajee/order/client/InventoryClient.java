d9d508287f7d410a65b320b6c6ca163d38006f8f
Sajeevan98 <sajee91098@gmail.com>
2025-07-12 13:26:19 +0530

    #10.7, Refactor code to use Spring's Http-Interface with RestClient instead of OpenFeign for external microservice interaction

--- e-commerce/server/order-service/src/main/java/com/project/sajee/order/client/InventoryClient.java
+++ e-commerce/server/order-service/src/main/java/com/project/sajee/order/client/InventoryClient.java
@@ -1,13 +1,10 @@
 package com.project.sajee.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${spring.inventory.client.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1.0/inventory")
+    @GetExchange("/api/v1.0/inventory")
     boolean isStock(@RequestParam String code, @RequestParam Integer quantity);
 }
