1c05cf3068ca42b5d957f5ca8e9be25c34d099ec
Sajeevan98 <sajee91098@gmail.com>
2025-07-12 10:20:46 +0530

    #9.9, add OpenAPI configuration for Order Service API documentation

---
+++ e-commerce/server/order-service/src/main/java/com/project/sajee/order/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.project.sajee.order.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI orderServiceApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order-Service-API")
+                        .description("REST API for order microservice")
+                        .version("v1.0")
+                        .license(new License().name("maven.apache-4.0.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("If you have any concern contact me!")
+                        .url("https://sajeevan-profile-web.netlify.app/"));
+    }
+}
