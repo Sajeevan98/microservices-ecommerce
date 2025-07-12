569a5b1fa1740c247d62b79a96f502a8a05b6225
Sajeevan98 <sajee91098@gmail.com>
2025-07-12 12:30:21 +0530

    updated public access urls

--- e-commerce/server/api-gateway/src/main/java/com/project/sajee/gateway/config/SecurityConfig.java
+++ e-commerce/server/api-gateway/src/main/java/com/project/sajee/gateway/config/SecurityConfig.java
@@ -9,11 +9,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String [] freeResourcesURLs= {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/api-docs/**",
+            "/aggregate/**" // Allows requests like /aggregate/product-service/v3/api-docs
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-        return security.authorizeHttpRequests(authorize-> authorize.anyRequest()
-                .authenticated())
-                .oauth2ResourceServer(oauth2-> oauth2.jwt(Customizer.withDefaults()))
-                .build();
+        return security.authorizeHttpRequests(authorize-> authorize
+                        .requestMatchers(freeResourcesURLs).permitAll()
+                        .anyRequest().authenticated())
+                        .oauth2ResourceServer(oauth2-> oauth2.jwt(Customizer.withDefaults()))
+                        .build();
     }
 }
